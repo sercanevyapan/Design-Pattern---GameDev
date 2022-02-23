7fee39db1cedd367128ed2edc5db912b4bb38a43
Sercan Evyapan <sevyapan@gmail.com>
2022-02-23 17:55:33 +0300

    Add Finite State Machines

---
+++ Finite State Machines/Assets/_Scripts/LocomotionFSM.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionFSM : MonoBehaviour
+{
+    enum State
+    {
+        Grounded,
+        InAir,
+        Crouching
+    }
+
+    State currentState = State.Grounded;
+
+    public void Jump()
+    {
+        switch (currentState)
+        {
+            case State.Grounded:
+                currentState = State.InAir;
+                break;
+            case State.Crouching:
+                currentState = State.Grounded;
+                break;
+        }
+    }
+
+    public void Fall(){
+        switch (currentState)
+        {
+            case State.Grounded:
+                currentState = State.InAir;
+                break;
+            case State.Crouching:
+                currentState = State.InAir;
+                break;
+        }
+    }
+
+    public void Land(){
+        switch (currentState)
+        {
+            case State.InAir:
+                currentState = State.Grounded;
+                break;
+            
+        }
+    }
+
+    public void Crouch(){
+         switch (currentState)
+        {
+            case State.Grounded:
+                currentState = State.Crouching;
+                break;
+            case State.Crouching:
+                currentState = State.Grounded;
+                break;
+        }
+    }
+}
