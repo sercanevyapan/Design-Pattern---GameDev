8284963705e190db744d43c60f415d2ae0e0adcf
Sercan Evyapan <55910627+sercanevyapan@users.noreply.github.com>
2022-03-25 10:38:14 +0300

    Decorator Pattern

---
+++ Decorator Pattern/Assets/AbilityRunner.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityRunner : MonoBehaviour
+{
+
+    [SerializeField]
+    IAbility currentAbility =
+
+        new DelayedDecorator(
+            new RageAbility()
+
+        );
+
+    public void UseAbility()
+    {
+
+        currentAbility.Use(gameObject);
+
+    }
+}
+
+public interface IAbility
+{
+    void Use(GameObject currentGameObject);
+}
+
+public abstract class BaseAbilitySO: ScriptableObject, IAbility
+{
+    public abstract void Use(GameObject currentGameObject);
+}
+
+public class DelayedDecorator : IAbility
+{
+    private IAbility wrappedAbility;
+
+    public DelayedDecorator(IAbility wrappedAbility)
+    {
+        this.wrappedAbility = wrappedAbility;
+    }
+
+    public void Use(GameObject currentGameObject)
+    {
+        // TODO some delaying functionality
+        wrappedAbility.Use(currentGameObject);
+    }
+}
+
+
+public class RageAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("I'm always angry");
+    }
+}
+
+public class FireBallAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Launch Fireball");
+    }
+}
+
+public class HealAbility : IAbility
+{
+    public void Use(GameObject currentGameObject)
+    {
+        Debug.Log("Here eat this!");
+    }
+}
